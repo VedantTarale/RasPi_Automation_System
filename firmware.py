28211944d47889317bcad0e9415de5926e76b75c
Vedant Tarale <vedanttarale10@gmail.com>
2023-10-28 16:18:40 +0530

    added waterpump code

--- firmware.py
+++ firmware.py
@@ -7,6 +7,12 @@ i2c = busio.I2C(board.SCL, board.SDA)
 bmp = 0x76
 NodeMCU = 0x12
 
+pump = 16
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pump, GPIO.OUT)
+
 def initialize():
     print("I2C Devices Found:", [hex(i) for i in i2c.scan()])
     if bmp not in i2c.scan():
@@ -119,8 +125,28 @@ def get_NodeMCU_Reading():
     i2c.writeto(NodeMCU, bytes([0x00]), stop=False)
     result = bytearray(1)
     i2c.readfrom_into(NodeMCU, result)
+
+    global water_reading
+    water_reading = int.from_bytes(result, "big")
+
     print("value: ", int.from_bytes(result, "big"))
     time.sleep(1)
+
+def pump_water():
+    global water_reading
+    final_val = 160 #to be modified later
+    margin = 5 #to be modified later
+    GPIO.output(pump,0)
+    get_NodeMCU_Reading()
+    try:
+        while (final_val - water_reading) > margin:
+            GPIO.output(pump,1)
+            time.sleep(1)
+            get_NodeMCU_Reading()
+        print("done")
+        GPIO.output(pump,0)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 def get_reading():
     #calibration_data = calibrate()
